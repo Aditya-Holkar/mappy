f2a21cd55a27df9d4f388d0afc8f80a440fcebfa
rohan1522 <123460179+rohan1522@users.noreply.github.com>
2024-02-27 11:01:36 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { firebaseConfig } from './firebase-config';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/firebase-config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBKxLNANtuu69PEaJu3rXSxwcrwA2SmFVo",
+  authDomain: "kgis-658b4.firebaseapp.com",
+  projectId: "kgis-658b4",
+  storageBucket: "kgis-658b4.appspot.com",
+  messagingSenderId: "396734589054",
+  appId: "1:396734589054:web:7285c9d8174a086423902f",
+  measurementId: "G-HF6WYN45QJ"
+  };
+  
